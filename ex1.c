f6c7188db7cfc81abe204859cea7209e9c099f1f
Tarek S Hafez <thafez@ymail.com>
2015-12-01 13:38:44 -0600

    Updated ex1 and ex1.c for extra credit. Added Makefile from ex2

--- ex1.c
+++ ex1.c
@@ -8,5 +8,13 @@ int main(int argc, char *argv[])
     // this is also a comment
     printf("You are %d miles away.\n", distance);
 
+    printf("The treasure is %d meters to the north.\n", distance);
+
+    printf("There are %d miles until you arrive at your destination.\n", distance);
+
+    printf("We have %d bottles of beer on the wall.\n", distance);
+
+    printf("%d hours left before rescue.\n", distance);
+
     return 0;
 }
